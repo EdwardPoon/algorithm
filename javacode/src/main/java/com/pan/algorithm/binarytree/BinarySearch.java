1a489ed2b2a04433a698c4646362f382fab10446
EdwardPoon <almar1701@hotmail.com>
2021-02-12 01:45:54 +0800

    binary search for the first element

--- javacode/src/main/java/com/pan/algorithm/binarytree/BinarySearch.java
+++ javacode/src/main/java/com/pan/algorithm/binarytree/BinarySearch.java
@@ -3,7 +3,7 @@ package com.pan.algorithm.binarytree;
 public class BinarySearch {
 
     public static void main(String[] args){
-        int[] arr = new int[8];
+        int[] arr = new int[12];
         arr[0] = 1;
         arr[1] = 2;
         arr[2] = 3;
@@ -11,17 +11,20 @@ public class BinarySearch {
         arr[4] = 5;
         arr[5] = 6;
         arr[6] = 7;
-        arr[7] = 8;
-        System.out.println("index="+binarySearch(arr, 1));
+        arr[7] = 7;
+        arr[8] = 7;
+        arr[9] = 7;
+        arr[10] = 9;
+        arr[11] = 9;
+        System.out.println("index="+binarySearchTheFirst(arr, 7));
     }
 
     private static int binarySearch(int[] array, int value) {
-        int index = -1;
         int low = 0;
         int high = array.length - 1;
 
         while (low <= high){
-            int mid = low + (high - low) >> 1;
+            int mid = low + ((high - low) >> 1);
             System.out.println("high="+ high+",low="+ low+",mid=" + mid);
             if (array[mid] == value) {
                 return mid;
@@ -32,6 +35,28 @@ public class BinarySearch {
             }
         }
 
-        return index;
+        return -1;
+    }
+    // when there are duplicate number in the array, find the first matching number
+    private static int binarySearchTheFirst(int[] array, int value) {
+
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high){
+            int mid = low + ((high - low) >> 1);
+            System.out.println("high="+ high+",low="+ low+",mid=" + mid);
+            if (array[mid] < value){
+                low = mid + 1;
+            } else if (array[mid] > value) {
+                high = mid - 1;
+            } else {
+                if ((mid==0)|| array[mid-1] != value)
+                    return mid;
+                else
+                    high = mid -1;
+            }
+        }
+        return -1;
     }
 }
