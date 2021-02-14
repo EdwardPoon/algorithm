92b2c1fcf077ef6ffded0340d517df989ca58a45
EdwardPoon <almar1701@hotmail.com>
2021-02-15 02:49:17 +0800

    find prime factors

---
+++ javacode/src/main/java/com/pan/algorithm/FindPrimeFactors.java
@@ -0,0 +1,41 @@
+package com.pan.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindPrimeFactors {
+
+    public static void main(String... args) {
+
+        System.out.println(Math.sqrt(35));
+        System.out.println((new FindPrimeFactors()).findPrimeFactors(791));
+
+    }
+
+    public List<Integer> findPrimeFactors(int number) {
+        List<Integer> factors = new ArrayList<Integer>();
+        while (number % 2 == 0) {
+            factors.add(2);
+            number = number /2;
+
+        }
+        // Every composite number has at least one prime factor less than or equal to square root of itself.
+
+        for (int i = 3; i <= Math.sqrt(number); i=i+2) {
+            System.out.println("i="+i);
+            while  (number % i == 0) {
+                factors.add(i);
+
+                number /= i;
+                System.out.println("number="+number);
+
+            }
+
+        }
+        // add the remain number which is the result of divided by last i
+        if (number > 2)
+            factors.add(number);
+
+        return factors;
+    }
+}
---
+++ javacode/src/main/java/com/pan/algorithm/dynamicprogramming/NthFibonacci.java
@@ -0,0 +1,30 @@
+package com.pan.algorithm.dynamicprogramming;
+
+// Calculate the N’th Fibonacci number.
+public class NthFibonacci {
+
+    public static void main(String... args) {
+        int n = 6;
+        System.out.println((new NthFibonacci()).findNthFibonacciDynamic(n));
+    }
+    // time complexity O(2^n)
+    public int findNthFibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n ==1) {
+            return 1;
+        } else {
+            return findNthFibonacci(n -1 ) + findNthFibonacci(n -2 );
+        }
+    }
+    // time complexity O(n)
+    public int findNthFibonacciDynamic(int n) {
+        int[] fibonacci = new int[n+1];
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+        for (int i = 2;i <= n; i++) {
+            fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+        }
+        return fibonacci[n];
+    }
+}
