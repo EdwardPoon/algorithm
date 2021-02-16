2c0eea4d4aec51d80dbf9d781331d27b406cd068
EdwardPoon <almar1701@hotmail.com>
2021-02-16 23:39:42 +0800

    create MinimumLengthSubArray

---
+++ javacode/src/main/java/com/pan/algorithm/MinimumLengthSubArray.java
@@ -0,0 +1,14 @@
+package com.pan.algorithm;
+
+// Given an array, find a subarray such the sum of the elements in subarray is greater than or equal to target
+//value. Find the minimus length of such subarray.
+public class MinimumLengthSubArray {
+
+    public static void main(String... args) {
+
+    }
+
+    // return a class with startIndex and length
+    // traverse the array, i , sum(from i to j), if sum >= given target, store the smallest value of the subArray, next i,
+    // if array[i] < smallest value, skip
+}
